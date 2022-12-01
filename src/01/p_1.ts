70fc8f68804ff8b409d00be636ffad4b26ca647a
JcbSm <jacob.ted.seaman@gmail.com>
2022-12-01 14:41:48 +0000

    Day 1 Part 1

---
+++ src/01/p_1.ts
@@ -0,0 +1,3 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+console.log(Math.max(...((readFileSync(path.resolve(__dirname, './input.txt'))).toString()).split('\r\n\r\n').map((str) => str.split('\r\n').map((str) => parseInt(str)).reduce((a,c) => a+c,0))));
