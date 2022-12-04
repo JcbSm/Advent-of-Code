80d14098580e1ee935c82076eb337dd16f9599d6
JcbSm <jacob.ted.seaman@gmail.com>
2022-12-04 14:16:07 +0000

    one-lined

--- src/01/p_2.ts
+++ src/01/p_2.ts
@@ -1,2 +1,1 @@
-import { readFileSync } from 'fs'; import path from 'path';
-console.log(((readFileSync(path.resolve(__dirname, './input.txt'))).toString()).split('\r\n\r\n').map((str) => str.split('\r\n').map((str) => parseInt(str)).reduce((a,c) => a+c,0)).sort((a,b)=>b-a).slice(0,3).reduce((a,c)=>a+c,0));
+console.log(((require('fs').readFileSync(require('path').resolve(__dirname, './input.txt'))).toString()).split('\r\n\r\n').map((str:string) => str.split('\r\n').map((str) => parseInt(str)).reduce((a,c)=>a+c,0)).sort((a:number,b:number)=>b-a).slice(0,3).reduce((a:number,c:number)=>a+c,0));
--- src/02/p_2.ts
+++ src/02/p_2.ts
@@ -1,2 +1,1 @@
-import { readFileSync } from "fs"; import { resolve } from 'path';
-console.log((readFileSync(resolve(__dirname,'input.txt'))).toString().split("\r\n").map(s => s.split(" ").map(c=>{return{'A':0,'B':1,'C':2,'X':0,'Y':1,'Z':2}[c]!})).map(t=>[[3,1,2],[4,5,6],[8,9,7]][t[1]][t[0]]).reduce((a,c)=>a+c,0));
+console.log((require('fs').readFileSync(require('path').resolve(__dirname,'input.txt'))).toString().split("\r\n").map((s:string) => s.split(" ").map(c=>{return{'A':0,'B':1,'C':2,'X':0,'Y':1,'Z':2}[c]!})).map((t:number[])=>[[3,1,2],[4,5,6],[8,9,7]][t[1]][t[0]]).reduce((a:number,c:number)=>a+c,0));
