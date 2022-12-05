ecb392b82a7b94d72ed099dbd929f5be4ba655e3
JcbSm <jacob.ted.seaman@gmail.com>
2022-12-05 17:40:04 +0000

    Create p_2_oneline.ts

---
+++ src/05/p_2_oneline.ts
@@ -0,0 +1,3 @@
+let stacks = [[],[],[],[],[],[],[],[],[]].map((v,i,a:string[][])=>require('fs').readFileSync(require('path').resolve(__dirname,'input.txt')).toString().split("\r\n").slice(0,8).map((s:string)=>s.charAt(4*i+1) == ' ' ? 0 : a[i].unshift(s.charAt(4*i+1))).map((n:number)=>a[i])[0])
+require('fs').readFileSync(require('path').resolve(__dirname,'input.txt')).toString().split("\r\n").slice(10).map((s:string)=>s.split(' ').map(s=>Number(s)).filter((n:number)=>!isNaN(n))).forEach((c:number[])=>stacks[c[2]-1].push(...stacks[c[1]-1].splice(stacks[c[1]-1].length-c[0],c[0]).reverse()))
+console.log(stacks.map(s=>s.join("")));
