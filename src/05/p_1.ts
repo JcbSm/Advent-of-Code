5b34430ec19b718b19fe02624c274b24c80b2774
JcbSm <jacob.ted.seaman@gmail.com>
2022-12-05 16:23:00 +0000

    Day 5 (not quite one line :( )

---
+++ src/05/p_1.ts
@@ -0,0 +1,10 @@
+// Read data
+const data = require('fs').readFileSync(require('path').resolve(__dirname,'input.txt')).toString().split("\r\n")
+// Initialise (crate) stack
+let stack:string[][] = [[],[],[],[],[],[],[],[],[]]
+let init:string[] = data.slice(0,8);
+stack.forEach((v,i,a)=> init.forEach(s=>s.charAt(4*i+1) == ' ' ? null : a[i].unshift(s.charAt(4*i+1))));
+// Each move
+let commands = data.slice(10).map((s:string)=>s.split(' ').map(s=>Number(s)).filter((n:number)=>!isNaN(n)));
+commands.forEach((c:number[])=>stack[c[2]-1].push(...stack[c[1]-1].splice(stack[c[1]-1].length-c[0], c[0]).reverse()))
+console.log(stack.map(a=>a.join("")));
